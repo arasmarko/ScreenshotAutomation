ce3603363974423eada32fd8febe07615a101e96
arasmarko <markoaras@yahoo.com>
2020-03-24 09:06:46 +0100

    Project init

---
+++ ScreenshotAutomation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenshotAutomation
+//
+//  Created by Marko Aras on 24/03/2020.
+//  Copyright © 2020 Codable Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScreenshotAutomationUITests/ScreenshotAutomationUITests.swift
@@ -0,0 +1,24 @@
+//
+//  ScreenshotAutomationUITests.swift
+//  ScreenshotAutomationUITests
+//
+//  Created by Marko Aras on 24/03/2020.
+//  Copyright © 2020 Codable Studio. All rights reserved.
+//
+
+import XCTest
+
+class ScreenshotAutomationUITests: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+    override func tearDown() {
+   
+    }
+
+    func testHome() {
+        XCTAssertTrue(true)
+    }
+}
